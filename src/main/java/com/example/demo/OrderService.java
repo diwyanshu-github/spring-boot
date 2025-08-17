d998fd01e2acd42269024d9b71c8bf512472bc70
Russell <diwyanshucool@gmail.com>
2025-08-17 14:39:54 +0530

    using qualifier to choose beans

--- src/main/java/com/example/demo/OrderService.java
+++ src/main/java/com/example/demo/OrderService.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
     private PaymentService paymentService;
-    public OrderService(PaymentService paymentService){
+    public OrderService(@Qualifier("paypal") PaymentService paymentService){
         this.paymentService = paymentService;
     }
     public void processPayment(){
