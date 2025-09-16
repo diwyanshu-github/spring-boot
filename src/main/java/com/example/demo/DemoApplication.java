5bba1c853b9b5c012bbfac831a4fb1d2bc03e5c9
Russell <diwyanshucool@gmail.com>
2025-09-16 11:40:47 +0530

    using repositories

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-//        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-
-        SpringApplication.run(DemoApplication.class, args);
-
+        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        var repo = context.getBean(UserRepository.class);
+        var user = User.builder().name("john2").email("john@gmail.com").password("abc").build();
+        repo.save(user);
+        repo.deleteAll();
+        var getUser = repo.findById(1L).orElseThrow();
+//        System.out.println(getUser.getEmail());
     }
 }
